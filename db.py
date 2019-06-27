ec8287a634971c6352ce4d6c6dd7adb0b250b90f
dsowinski2 <dsowinski2@gmail.com>
2019-06-27 13:32:51 +0200

    Heroku

--- db.py
+++ db.py
@@ -9,14 +9,12 @@ from zipfile import ZipFile
 
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 
 class movies(db.Model):
     __tablename__ = 'movies'
-
     movieId = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), nullable=False)
     genres = db.Column(db.String(100), nullable=False)
