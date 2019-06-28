2e237aaebcc03cd21cc55f23be920d8c4ff4eafd
dsowinski2 <dsowinski2@gmail.com>
2019-06-28 18:38:23 +0200

    Database create file

--- db.py
+++ db.py
@@ -2,19 +2,25 @@ import urllib.request
 from io import BytesIO
 
 import pandas as pd
-from flask import Flask, render_template,request,send_file,jsonify
+from flask import (
+    Flask,
+    render_template,
+    request,
+    send_file,
+    jsonify,
+)
 from flask_sqlalchemy import SQLAlchemy
 import re
 from zipfile import ZipFile
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
 
 class ratings(db.Model):
-    __tablename__ = 'ratings'
+    __tablename__ = "ratings"
     index = db.Column(db.Integer, primary_key=True)
     userId = db.Column(db.Integer, nullable=False)
     movieId = db.Column(db.Integer, nullable=False)
@@ -26,20 +32,21 @@ class ratings(db.Model):
 
 
 class movies(db.Model):
-    __tablename__ = 'movies'
+    __tablename__ = "movies"
     movieId = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(64), nullable=False)
     genres = db.Column(db.String(100), nullable=False)
     year = db.Column(db.Integer, nullable=False)
 
-
-
     def __repr__(self):
         return f"movies('{self.movieId}','{self.title}','{self.genres}','{self.year}')"
 
+
 class links(db.Model):
-    __tablename__ = 'links'
-    index = db.Column(db.Integer,nullable=False, primary_key=True)
+    __tablename__ = "links"
+    index = db.Column(
+        db.Integer, nullable=False, primary_key=True
+    )
     movieId = db.Column(db.Integer, nullable=False)
     imdbId = db.Column(db.Integer, nullable=False)
     tmdbId = db.Column(db.Float)
@@ -49,8 +56,10 @@ class links(db.Model):
 
 
 class tags(db.Model):
-    __tablename__ = 'tags'
-    index = db.Column(db.Integer, nullable=False, primary_key=True)
+    __tablename__ = "tags"
+    index = db.Column(
+        db.Integer, nullable=False, primary_key=True
+    )
     userId = db.Column(db.Integer, nullable=False)
     movieId = db.Column(db.Integer, nullable=False)
     tag = db.Column(db.String(64), nullable=False)
@@ -58,4 +67,3 @@ class tags(db.Model):
 
     def __repr__(self):
         return f"tags('{self.movieId}','{self.tag}')"
-
